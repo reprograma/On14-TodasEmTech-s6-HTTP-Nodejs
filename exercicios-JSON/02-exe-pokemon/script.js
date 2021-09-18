e8e6fae247cf53c610d4655ad79fc71f60a64d8c
Estefanya <estefanya.nascimento@outlook.com>
2021-09-18 16:42:24 -0300

    atuaizacao

--- exercicios-JSON/02-exe-pokemon/script.js
+++ exercicios-JSON/02-exe-pokemon/script.js
@@ -31,3 +31,18 @@ const data = [{
 
 // COMEÇA O EXERCÍCIO
 
+for (let i=0; i <= data.length-1; i++){
+    let pokemon = data[i]
+
+   console.log(pokemon.name)
+   console.log(pokemon.description)
+   console.log(pokemon.art_url)
+   console.log(pokemon.types)
+
+  }
+
+let listaTipos = pokemon.types
+for (let j =0; j <= listaTipos.length-1; j++){
+
+    console.log(listaTipos[j])
+}
